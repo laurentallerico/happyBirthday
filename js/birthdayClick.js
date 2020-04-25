451c60c947dece15044c27fec499bf8b5f089cdf
Lauren Tallerico <lat13@live.missouristate.edu>
2020-04-25 12:52:30 -0500

    initial commit

---
+++ js/birthdayClick.js
@@ -0,0 +1,17 @@
+
+
+//document.onload.addEventListener
+let bunch = ["Happy", "Birthday", "Tony"]
+let option = 0
+let phrase = bunch[option]
+
+document.getElementsByTagName('body').addEventListener("click", function () {
+    document.getElementById("main").innerHTML = phrase;
+    if (option == 2) option = 0;
+    else option++;
+});
+
+// clear text box on DOM load
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('main').innerHTML = "";
+});
