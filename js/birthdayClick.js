5b9f99172ad93004d0ae6a2814e08bf4bc75a24d
Lauren Tallerico <lat13@live.missouristate.edu>
2020-04-25 14:03:59 -0500

    phrase needs updating each time, duh

--- js/birthdayClick.js
+++ js/birthdayClick.js
@@ -3,9 +3,10 @@
 //document.onload.addEventListener
 let bunch = ["Happy", "Birthday", "Tony"]
 let option = 0
-let phrase = bunch[option]
+
 
 document.getElementById('button').addEventListener("click", function () {
+    let phrase = bunch[option]
     document.getElementById('main').innerHTML = phrase;
     if (option == 2) option = 0;
     else option++;
