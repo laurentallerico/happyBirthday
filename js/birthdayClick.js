295230907e37cbe0176043b4cb1350ad77611c79
Lauren Tallerico <lat13@live.missouristate.edu>
2020-04-26 13:18:18 -0500

    made button move, added song

--- js/birthdayClick.js
+++ js/birthdayClick.js
@@ -1,18 +1,20 @@
 
 
 //document.onload.addEventListener
-let bunch = ["Happy", "Birthday", "Tony"]
+let song = ["Hap", "py", "birth", "day", "to", "you!", "Hap", "py", "birth", "day", "to", "you!", "Happ", "y",
+    "y", "y", "y", "y", "y", "y", "bir", "r", "r", "th", "daaaa", "y", "yy", "yyy", "yyyy", "dear" "To", "Tony",
+    "Hap", "py", "birth", "day", "to", "YOU!"];
 let option = 0
 
 
-document.getElementById('button').addEventListener("click", function () {
-    let phrase = bunch[option]
-    document.getElementById('main').innerHTML = phrase;
-    if (option == 2) option = 0;
+document.addEventListener("click", function (event) {
+    let button = document.getElementById("button");
+    let phrase = song[option]
+    button.innerHTML = phrase;
+    if (option == song.length) option = 0;
     else option++;
-});
-
-// clear div on DOM load
-document.addEventListener("DOMContentLoaded", function () {
-    //document.getElementById('main').innerHTML = "";
+    let x = event.clientX
+    let y = event.clientY
+    button.style.left = x + "px"
+    button.style.top = y + "px"
 });
