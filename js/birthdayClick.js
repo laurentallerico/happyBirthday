ff92adce8a7b4b94ced17c83b6eb27ce37044ae5
Lauren Tallerico <lat13@live.missouristate.edu>
2020-04-27 18:57:30 -0500

    bettered song

--- js/birthdayClick.js
+++ js/birthdayClick.js
@@ -2,13 +2,12 @@
 
 //document.onload.addEventListener
 let song = ["Hap-", "-py", "birth-", "-day", "to", "you!", "Hap-", "-py", "birth-", "-day", "to", "you!", "Hap-", "-py",
-    "bi-", "-i-", "-ii-", "-iii-", "-iiii-", "-iiiii-", "-iiiiii-", "-irth-", "-daaaay-", "-y", "-yy", "-yyy",
-    "-yyyy", "dear", "To-", "-o-", "-oo-", "-ooo-", "-nyyyyyyyyy!", "Hap-", "-py", "birth-", "-day", "to", "YOU!"];
+    "bi-", "-i-", "-ii-", "-iii-", "-iiii-", "-iiiii-", "-iiiiii-", "-irth-", "-daaaay-", "dear", "To-", "-nyyyyyyyyy!",
+    "Hap-", "-py", "birth-", "-day", "to", "YOU!"];
 let chords = ["re.mp3", "re.mp3", "mi.mp3", "re.mp3", "sol.mp3", "faSharp.mp3", "re.mp3", "re.mp3",
     "mi.mp3", "re.mp3", "la.mp3", "sol.mp3", "re.mp3", "re.mp3", "re2.mp3", "re2.mp3",
-    "re2.mp3", "re2.mp3", "re2.mp3", "re2.mp3", "re2.mp3", "re2.mp3", "si.mp3", "si.mp3", "si.mp3",
-    "si.mp3", "si.mp3", "sol.mp3", "faSharp.mp3", "faSharp.mp3", "faSharp.mp3", "faSharp.mp3", "mi.mp3",
-    "do.mp3", "do.mp3", "si.mp3", "sol.mp3", "la.mp3", "sol.mp3"];
+    "re2.mp3", "re2.mp3", "re2.mp3", "re2.mp3", "re2.mp3", "re2.mp3", "si.mp3", "si.mp3", "sol.mp3",
+    "faSharp.mp3", "mi.mp3", "do.mp3", "do.mp3", "si.mp3", "sol.mp3", "la.mp3", "sol.mp3"];
 let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 let colorOption = 0;
 let option = 0;
