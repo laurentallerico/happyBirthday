00bbac5dfa427ea20863b27ccb1b51dde5d0f247
Lauren Tallerico <lat13@live.missouristate.edu>
2020-04-27 16:59:04 -0500

    added music notes

--- js/birthdayClick.js
+++ js/birthdayClick.js
@@ -2,14 +2,21 @@
 
 //document.onload.addEventListener
 let song = ["Hap-", "-py", "birth-", "-day", "to", "you!", "Hap-", "-py", "birth-", "-day", "to", "you!", "Hap-", "-py",
-    "bi-", "-i-", "-i-", "-i-", "-i-", "-i-", "-i-", "-irth-", "-daaaay-", "-y", "-yy", "-yyy", "-yyyy", "dear", "To-", "-o-",
-    "-oo-", "-ooo-", "-nyyyyyyyyy!", "Hap-", "-py", "birth-", "-day", "to", "YOU!"];
+    "bi-", "-i-", "-ii-", "-iii-", "-iiii-", "-iiiii-", "-iiiiii-", "-irth-", "-daaaay-", "-y", "-yy", "-yyy",
+    "-yyyy", "dear", "To-", "-o-", "-oo-", "-ooo-", "-nyyyyyyyyy!", "Hap-", "-py", "birth-", "-day", "to", "YOU!"];
+let chords = ["Re.mp3", "Re.mp3", "Mi.mp3", "Re.mp3", "Sol.mp3", "Fa#.mp3", "Re.mp3", "Re.mp3",
+    "Mi.mp3", "Re.mp3", "La.mp3", "Sol.mp3", "Re.mp3", "Re.mp3", "Re2.mp3", "Re2.mp3",
+    "Re2.mp3", "Re2.mp3", "Re2.mp3", "Re2.mp3", "Re2.mp3", "Re2.mp3", "Si.mp3", "Si.mp3", "Si.mp3",
+    "Si.mp3", "Si.mp3", "Sol.mp3", "Fa#.mp3", "Fa#.mp3", "Fa#.mp3", "Fa#.mp3", "Mi.mp3",
+    "Do.mp3", "Do.mp3", "Si.mp3", "Sol.mp3", "La.mp3", "Sol.mp3"];
 let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 let colorOption = 0;
 let option = 0;
 
 
 document.addEventListener("click", function (event) {
+    var chord = new Audio("music/" + chords[option]);
+    chord.play();
     let phrase = song[option];
     let color = colors[colorOption];
     let button = document.createElement("button");
